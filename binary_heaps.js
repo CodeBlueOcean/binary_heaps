41c7002372b3b93c68ace68a3316548a59ba613c
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-04-18 20:49:32 -0500

    Add files via upload

---
+++ binary_heaps.js
@@ -0,0 +1,16 @@
+// Interview Data Structures
+// Binary Heap
+// Lookup O(n)
+// Insert O(log N)
+// Delete O(log N)
+
+// Binary Heap
+//        101
+//    72       33
+// 2    45   5    1
+
+// Worst Case: O(log n)
+// Best Case: O(I)
+
+// Memory Heap != Data Structure Heap
+
